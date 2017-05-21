398f0596c5bcf782e8e7087780449d1595e9b0d5
alexrewrew <alexrewrew@MacBook-Pro-Oleksandr.local>
2017-05-21 12:30:05 +0300

    Almost

--- app/js/main.js
+++ app/js/main.js
@@ -11027,6 +11027,34 @@ $(document).ready(function () {
         $('.nav-display').slideToggle();
     });
 
+    var sound1 = $("#audio1")[0];
+    var sound2 = $("#audio2")[0];
+    var sound3 = $("#audio3")[0];
+    var sound4 = $("#audio4")[0];
+    $(".note-1").mouseenter(function() {
+            sound1.pause();
+            sound1.play();
+        });
+    $(".note-2").mouseenter(function() {
+            sound2.pause();
+            sound2.play();
+        });
+    $(".note-3").mouseenter(function() {
+            sound3.pause();
+            sound3.play();
+        });
+    $(".note-4").mouseenter(function() {
+            sound4.pause();
+            sound4.play();
+        });
+    $(".sound-circle--sound").click(function() {
+            sound4.pause();
+            sound4.play();
+        });
+    $('.sound-circle--play').click(function(){
+        
+    });
+
     //responsive scripts
     if (window.matchMedia('(max-width: 767px)').matches) {
         
