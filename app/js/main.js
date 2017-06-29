630090cf2f489cfafc3acbd0c6de37fc92ac330b
alexrewrew <alexrewrew@MacBook-Pro-Oleksandr.local>
2017-06-29 13:24:28 +0300

    Drumbum

--- app/js/main.js
+++ app/js/main.js
@@ -11032,39 +11032,92 @@ $(document).ready(function () {
         $('.sound-circle--play svg').toggleClass('hidden-block');
     });
 
-    var sound1 = $("#audio1")[0];
-    var sound2 = $("#audio2")[0];
-    var sound3 = $("#audio3")[0];
-    var sound4 = $("#audio4")[0];
+    var sound11 = $("#audio11")[0];
+    var sound12 = $("#audio12")[0];
+    var sound13 = $("#audio13")[0];
+    var sound14 = $("#audio14")[0];
+    var sound15 = $("#audio14")[0];
+    var sound21 = $("#audio21")[0];
+    var sound22 = $("#audio22")[0];
+    var sound23 = $("#audio23")[0];
+    var sound24 = $("#audio24")[0];
+    var sound25 = $("#audio24")[0];
 
     $(".note-1").mouseenter(function () {
         if ($(this).parent().hasClass("unmuted")) {
-            sound1.pause();
-            sound1.play();
+            sound11.pause();
+            sound11.currentTime = 0;
+            sound11.play();
         }
     });
-    $(".unmuted>.note-2").hover(function () {
+    $(".note-2").hover(function () {
         if ($(this).parent().hasClass("unmuted")) {
-            sound2.pause();
-            sound2.play();
+            sound12.pause();
+            sound12.currentTime = 0;
+            sound12.play();
         }
     });
-    $(".unmuted>.note-3").mouseenter(function () {
+    $(".note-3").mouseenter(function () {
         if ($(this).parent().hasClass("unmuted")) {
-            sound3.pause();
-            sound3.play();
+            sound13.pause();
+            sound13.currentTime = 0;
+            sound13.play();
         }
     });
-    $(".unmuted>.note-4").mouseenter(function () {
+    $(".note-4").mouseenter(function () {
         if ($(this).parent().hasClass("unmuted")) {
-            sound4.pause();
-            sound4.play();
+            sound14.pause();
+            sound14.currentTime = 0;
+            sound14.play();
+        }
+    });
+    $(".note-5").mouseenter(function () {
+        if ($(this).parent().hasClass("unmuted")) {
+            sound15.pause();
+            sound15.currentTime = 0;
+            sound15.play();
+        }
+    });
+    $(".note-6").mouseenter(function () {
+        if ($(this).parent().hasClass("unmuted")) {
+            sound21.pause();
+            sound21.currentTime = 0;
+            sound21.play();
+        }
+    });
+    $(".note-7").hover(function () {
+        if ($(this).parent().hasClass("unmuted")) {
+            sound22.pause();
+            sound22.currentTime = 0;
+            sound22.play();
+        }
+    });
+    $(".note-8").mouseenter(function () {
+        if ($(this).parent().hasClass("unmuted")) {
+            sound23.pause();
+            sound23.currentTime = 0;
+            sound23.play();
+        }
+    });
+    $(".note-9").mouseenter(function () {
+        if ($(this).parent().hasClass("unmuted")) {
+            sound24.pause();
+            sound24.currentTime = 0;
+            sound24.play();
+        }
+    });
+    $(".note-10").mouseenter(function () {
+        if ($(this).parent().hasClass("unmuted")) {
+            sound25.pause();
+            sound25.currentTime = 0;
+            sound25.play();
         }
     });
     $(".sound-circle--sound").click(function () {
         if ($(this).parent().hasClass("unmuted")) {
-            sound4.pause();
-            sound4.play();
+            sound11.pause();
+            sound11.currentTime = 0;
+            sound11.play();
         }
     });
 
